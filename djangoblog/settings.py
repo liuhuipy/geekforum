a3fbc3a7f714cf3742c570308786b757aa099560
liuhui <liuhui_py@163.com>
2018-01-24 19:39:57 +0800

    update setting.py

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -48,8 +48,6 @@ INSTALLED_APPS = [
     'comments',
     'search',
     'users',
-
-
 ]
 
 
@@ -135,13 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 PAGE_NUM = 10
 
