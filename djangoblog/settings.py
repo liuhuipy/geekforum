a9a672013097a6d75df2ea8eb85049a03b2ed7e0
Liu Hui <liuhui_py@163.com>
2018-05-14 14:22:46 +0800

    Update settings.py

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -91,15 +91,22 @@ AUTH_USER_MODEL = 'users.UserProfile'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangoblog',
-	'USER': 'root',
-	'PASSWORD': 'aixocm',
-	'PORT': 3306,
-	'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
+
+# 使用mysql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'djangoblog',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'PORT': 3306,
+#         'HOST': '127.0.0.1',
+#     }
+# }
 
 
 # Password validation
